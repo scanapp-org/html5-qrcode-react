42275ed8ced37ae89ee383761bcd390295fc6e61
Mohit Tank <mtank@pfcflex.com>
2022-04-22 11:59:35 -0400

    Migrated from class component to functional component, and some code cleanup

--- src/ResultContainerPlugin.jsx
+++ src/ResultContainerPlugin.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function filterResults(results) {
+function filterResults (results) {
     let filteredResults = [];
     for (var i = 0; i < results.length; ++i) {
         if (i === 0) {
@@ -8,52 +8,50 @@ function filterResults(results) {
             continue;
         }
 
-        if (results[i].decodedText !== results[i-1].decodedText) {
+        if (results[i].decodedText !== results[i - 1].decodedText) {
             filteredResults.push(results[i]);
         }
     }
     return filteredResults;
 }
 
-class ResultContainerTable extends React.Component {
-    render() {
-        var results = filterResults(this.props.data);
-        return (
-            <table className={'Qrcode-result-table'}>
-                <thead>
-                    <tr>
-                        <td>#</td>
-                        <td>Decoded Text</td>
-                        <td>Format</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        results.map((result, i) => {
-                            console.log(result);
-                            return (<tr key={i}>
-                                <td>{i}</td>
-                                <td>{result.decodedText}</td>
-                                <td>{result.result.format.formatName}</td>
-                            </tr>);
-                        })
-                    }
-                </tbody>
-            </table>
-        );
-    }
-}
+const ResultContainerTable = ({ data }) => {
+    const results = filterResults(data);
+    return (
+        <table className={'Qrcode-result-table'}>
+            <thead>
+                <tr>
+                    <td>#</td>
+                    <td>Decoded Text</td>
+                    <td>Format</td>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    results.map((result, i) => {
+                        console.log(result);
+                        return (<tr key={i}>
+                            <td>{i}</td>
+                            <td>{result.decodedText}</td>
+                            <td>{result.result.format.formatName}</td>
+                        </tr>);
+                    })
+                }
+            </tbody>
+        </table>
+    );
+};
 
-class ResultContainerPlugin extends React.Component {
-    render() { 
-        let results = filterResults(this.props.results);
-        return (<div className='Result-container'>
-                <div className='Result-header'>Scanned results ({results.length})</div>
-                <div className='Result-section'>
-                    <ResultContainerTable data={this.props.results} />
-                </div>
-            </div>);
-    }
-}
+const ResultContainerPlugin = (props) => {
+    const results = filterResults(props.results);
+    return (
+        <div className='Result-container'>
+            <div className='Result-header'>Scanned results ({results.length})</div>
+            <div className='Result-section'>
+                <ResultContainerTable data={results} />
+            </div>
+        </div>
+    );
+};
 
 export default ResultContainerPlugin;

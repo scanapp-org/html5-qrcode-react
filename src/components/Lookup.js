5fc607d166548afebb5deb82cebdef17a0cadfc2
Thomas Aurelius <teg824@hotmail.com>
2022-06-29 00:58:03 -0500

    m

---
+++ src/components/Lookup.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Lookup = () => {
+   const API_URL = 'https://www.googleapis.com/books/v1/volumes?q=isbn:'
+   
+  return (
+    <div>Lookup</div>
+
+
+   https://www.googleapis.com/books/v1/volumes?q=isbn:9781408810552
+
+
+
+  )
+}
+
+export default Lookup

f113bdccaa130035002e2e23535baba7740345f3
Thomas Aurelius <teg824@hotmail.com>
2022-06-29 17:58:28 -0500

    working on single state response

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import './App.css';
 
-
-
 import React from 'react';
 import Html5QrcodePlugin from './Html5QrcodePlugin.jsx'
 import ResultContainerPlugin from './ResultContainerPlugin.jsx'
 import Result2 from './Result2.jsx'
 import Lookup from './components/Lookup'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      decodedResults: []
-    }
-   
+export default function App(props) {
+
+
+  const [decodedResults, setDecodedResults] = React.useState()
+  
 
     // This binding is necessary to make `this` work in the callback.
-    this.onNewScanResult = this.onNewScanResult.bind(this);
-  }
+   //  this.onNewScanResult = this.onNewScanResult.bind(this);
+  
 
-  render() {
+  
     return (
       <div className="App">
         <section className="App-section">
@@ -32,27 +28,26 @@ class App extends React.Component {
             fps={10}
             qrbox={250}
             disableFlip={false}
-            qrCodeSuccessCallback={this.onNewScanResult}/>
-          <ResultContainerPlugin results={this.state.decodedResults} />
-          <Result2 results={this.state.decodedResults} />
-          <Lookup results={this.state.decodedResults} />
-          {console.log(this.state.decodedResults)}
+            qrCodeSuccessCallback={onNewScanResult}/>
+          <ResultContainerPlugin results={decodedResults} />
+          
+          <Lookup results={decodedResults} />
+          {console.log(decodedResults)}
         </section>
       </div>
     );
-  }
+  
 
-  onNewScanResult(decodedText, decodedResult) {
+  function onNewScanResult(decodedText, decodedResult)  {
     console.log(
       "App [result]", decodedResult);
 
+      console.log("State :" + decodedResults)
+
     // let decodedResults = this.state.decodedResults;
     // decodedResults.push(decodedResult);
-    this.setState((state, props) => {
-      state.decodedResults.push(decodedResult);
-      return state;
-    });
+
+    setDecodedResults( decodedResult)
+    
   }
 }
-
-export default App;
